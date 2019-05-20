5dad1cceec9679f301b37e0d378636fe786c5bdc
冯斌 <love@LAPTOP-RRNV5EOA>
2019-05-20 16:53:37 +0800

    bvbcvbcv

---
+++ lianxi2/src/main/java/com/bw/bean/Student.java
@@ -0,0 +1,37 @@
+package com.bw.bean;
+
+public class Student {
+	
+	private int tid;
+	
+	private String tname;
+	
+	private int age;
+
+	public int getTid() {
+		return tid;
+	}
+
+	public void setTid(int tid) {
+		this.tid = tid;
+	}
+
+	public String getTname() {
+		return tname;
+	}
+
+	public void setTname(String tname) {
+		this.tname = tname;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	
+
+}
